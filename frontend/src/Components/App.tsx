e5828b48174349f600460b3276e4bd809a234769
Dameon Laird <26032268+DameonL@users.noreply.github.com>
2025-09-10 22:24:43 +0000

    Added copyright notice

--- frontend/src/Components/App.tsx
+++ frontend/src/Components/App.tsx
@@ -1,15 +1,19 @@
-import { h } from "preact";
+import { Fragment, h } from "preact";
 import LoginButton from "./Authentication/LoginButton";
+import Footer from "./Footer";
 import Navbar from "./Navbar";
 import Providers from "./Providers";
 import ToastMessages from "./ToastMessages";
 
 export default function App() {
   return (
-    <Providers>
-      <ToastMessages />
-      <LoginButton />
-      <Navbar />
-    </Providers>
+    <>
+      <Providers>
+        <ToastMessages />
+        <LoginButton />
+        <Navbar />
+      </Providers>
+      <Footer />
+    </>
   );
 }
---
+++ frontend/src/Components/Footer.tsx
@@ -0,0 +1,5 @@
+import { h } from "preact";
+
+export default function Footer() {
+    return <footer class="footer">Copyright {new Date().getFullYear()} <a href="https://www.dameonlaird.com" target="_blank">Dameon Laird</a></footer>
+}
