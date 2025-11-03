47fafe4c83d4d298a4f436668055c10132d845a7
Dameon Laird <26032268+DameonL@users.noreply.github.com>
2025-11-03 22:42:39 +0000

    Added missing slash

--- frontend/src/Components/Authentication/msalConfig.ts
+++ frontend/src/Components/Authentication/msalConfig.ts
@@ -2,7 +2,7 @@ export const msalConfig = {
   auth: {
     clientId: "36148247-7edd-499a-b4d2-6f37380a4f57",
     authority: "https://login.microsoftonline.com/f02557e2-0f9a-4a24-8388-cac48fdc9332",
-    redirectUri: "https:/www.closing-connect.com/LoginSuccess",
+    redirectUri: "https://www.closing-connect.com/LoginSuccess",
   },
   cache: {
     cacheLocation: "localStorage",
