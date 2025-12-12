786cad9cc415960afd24d01ef6b493a9b6014616
Dameon Laird <26032268+DameonL@users.noreply.github.com>
2025-12-12 19:43:01 +0000

    Use vite to build library

---
+++ api/vite.config.js
@@ -0,0 +1,25 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  base: "./",
+	root: "src/",
+	build: {
+    lib: {
+      entry: "./index.ts",
+      name: "Closing-Connect API"
+    },
+		outDir: "./build/",
+    emptyOutDir: true,
+		minify: "false",
+		rollupOptions: {
+			output: {
+				entryFileNames: "index.js",
+			},
+		},
+	},
+	vite: {
+		define: {
+			"global": {},
+		}
+	}
+});
