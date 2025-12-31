9367f0fe6005d889dd1a44763386a09e41bdeb0e
Dameon Laird <26032268+DameonL@users.noreply.github.com>
2025-12-31 21:34:29 +0000

    Use development mode

--- frontend/src/Components/Authentication/msalConfig.ts
+++ frontend/src/Components/Authentication/msalConfig.ts
@@ -1,10 +1,12 @@
+const isDevelopment = (import.meta as any).env.MODE === "development";
+
 export const msalConfig = {
   auth: {
-    clientId: "36148247-7edd-499a-b4d2-6f37380a4f57",
-    // clientId: "7a31ed6d-49fa-44a8-b592-1edc1d046073",
-    authority: "https://login.microsoftonline.com/f02557e2-0f9a-4a24-8388-cac48fdc9332",
+    clientId: isDevelopment ? "7a31ed6d-49fa-44a8-b592-1edc1d046073" : "36148247-7edd-499a-b4d2-6f37380a4f57",
+    // clientId: "7a31ed6d-49fa-44a8-b592-1edc1d046073", -- New tenant clientId
+    authority: isDevelopment ? "https://closingconnect.ciamlogin.com/closingconnect.onmicrosoft.com/v2.0/" : "https://login.microsoftonline.com/f02557e2-0f9a-4a24-8388-cac48fdc9332",
     // authority: "https://closingconnect.ciamlogin.com/closingconnect.onmicrosoft.com/v2.0/",
-    redirectUri: "https://www.closing-connect.com/LoginSuccess",
+    redirectUri: isDevelopment ? `${window.location.origin}/LoginSuccess` : "https://www.closing-connect.com/LoginSuccess",
   },
   cache: {
     cacheLocation: "localStorage",
@@ -16,8 +18,7 @@ export const scopes = [
   "openid",
   "profile",
   "offline_access",
-//  "api://7a31ed6d-49fa-44a8-b592-1edc1d046073/ClosingConnect",
-  "api://36148247-7edd-499a-b4d2-6f37380a4f57/Invoke"
+  isDevelopment ? "api://7a31ed6d-49fa-44a8-b592-1edc1d046073/ClosingConnect" : "api://36148247-7edd-499a-b4d2-6f37380a4f57/Invoke"
 ];
 
 export const loginRequest = { scopes };
