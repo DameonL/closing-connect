79d17f183a691546ff6c186b8748cbef17c0579d
Dameon Laird <26032268+DameonL@users.noreply.github.com>
2025-12-31 22:34:07 +0000

    Migrated to new API location + tenant

--- frontend/src/Components/Authentication/ApiDefinitions.ts
+++ frontend/src/Components/Authentication/ApiDefinitions.ts
@@ -6,7 +6,7 @@ export const ApiDefinitions = {
     routes: ["vendor", "search"]
   },
   private: {
-    url: isDevelopment ? "https://api-private-enezg8cye2dcfkdh.westus2-01.azurewebsites.net/" : "https://api.closing-connect.com/",
+    url: "https://api-private-enezg8cye2dcfkdh.westus2-01.azurewebsites.net/",
     routes: ["vendor"]
   }
 } as const;
--- frontend/src/Components/Authentication/msalConfig.ts
+++ frontend/src/Components/Authentication/msalConfig.ts
@@ -2,11 +2,9 @@ const isDevelopment = (import.meta as any).env.MODE === "development";
 
 export const msalConfig = {
   auth: {
-    clientId: isDevelopment ? "7a31ed6d-49fa-44a8-b592-1edc1d046073" : "36148247-7edd-499a-b4d2-6f37380a4f57",
-    // clientId: "7a31ed6d-49fa-44a8-b592-1edc1d046073", -- New tenant clientId
-    authority: isDevelopment ? "https://closingconnect.ciamlogin.com/closingconnect.onmicrosoft.com/v2.0/" : "https://login.microsoftonline.com/f02557e2-0f9a-4a24-8388-cac48fdc9332",
-    // authority: "https://closingconnect.ciamlogin.com/closingconnect.onmicrosoft.com/v2.0/",
-    redirectUri: isDevelopment ? `${window.location.origin}/LoginSuccess` : "https://www.closing-connect.com/LoginSuccess",
+    clientId: "7a31ed6d-49fa-44a8-b592-1edc1d046073",
+    authority: "https://closingconnect.ciamlogin.com/closingconnect.onmicrosoft.com/v2.0/",
+    redirectUri: "https://www.closing-connect.com/LoginSuccess",
   },
   cache: {
     cacheLocation: "localStorage",
@@ -18,7 +16,7 @@ export const scopes = [
   "openid",
   "profile",
   "offline_access",
-  isDevelopment ? "api://7a31ed6d-49fa-44a8-b592-1edc1d046073/ClosingConnect" : "api://36148247-7edd-499a-b4d2-6f37380a4f57/Invoke"
+  "api://7a31ed6d-49fa-44a8-b592-1edc1d046073/ClosingConnect"
 ];
 
 export const loginRequest = { scopes };
